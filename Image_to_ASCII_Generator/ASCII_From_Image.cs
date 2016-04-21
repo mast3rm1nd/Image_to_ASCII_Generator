283368d8e91bed6cff986c0a61cf3b77f5f5b9e4
mastermind <cyberdude@mail.ru>
2016-04-21 16:50:37 +0300

    +

---
+++ Image_to_ASCII_Generator/ASCII_From_Image.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.IO;
+
+namespace Image_to_ASCII_Generator
+{
+    class ASCII_From_Image
+    {
+        private static string chars_by_density = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+
+        public static void Generate(string image_path, string result_path)
+        {          
+            var img = new Bitmap(image_path);
+
+            using (var stream_writer = new StreamWriter(result_path))
+            {
+                for(var y = 0; y < img.Height; y++)
+                {
+                    for(var x = 0; x < img.Width; x++)
+                    {
+                        var color = img.GetPixel(x, y);
+                        var brightness = Calculate_Brightness_from_Colour(color);
+                        var index_of_char = brightness / 255 * (chars_by_density.Length - 1);
+                        var char_to_place = chars_by_density[(int)Math.Round(index_of_char)];
+
+                        stream_writer.Write(char_to_place);
+                        stream_writer.Write(char_to_place);
+                    }
+                    stream_writer.WriteLine();
+                }
+            }
+        }
+
+
+        private static double Calculate_Brightness_from_Colour(Color c)
+        {
+            return (int)Math.Sqrt(
+                c.R * c.R * .241 +
+                c.G * c.G * .641 +
+                c.B * c.B * .068);
+        }
+    }
+}
---
+++ Image_to_ASCII_Generator/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//using Microsoft.Win32;
+
+namespace Image_to_ASCII_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var file_opening_dialog = new OpenFileDialog
+                {
+                    Filter = "Image files (*.*)|*.*",
+                    Title = "Choose image for conversion to ASCII",
+                    CheckFileExists = true
+                };
+
+                if (file_opening_dialog.ShowDialog() == DialogResult.OK) //после выбора файла с текстом для статистики
+                {
+                    var file_save_dialog = new SaveFileDialog
+                    {
+                        Filter = "Text file (*.txt)|*.txt",
+                        Title = "Choose place where result will be saved"
+                    };
+
+                    if (file_save_dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        ASCII_From_Image.Generate(file_opening_dialog.FileName, file_save_dialog.FileName);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+    }
+}
